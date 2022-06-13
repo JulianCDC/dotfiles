2a085ba1d82e74516783bc3929f1aca111583f40
JulianCDC <me@juliancdc.dev>
2022-06-13 10:52:28 +0200

    lun. 13 juin 2022 10:52:28 CEST

---
+++ settings/loadenv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+env_keys=`yq .env ~/.settings/config.yaml | yq 'keys[]'`
+
+for key in $env_keys; do
+    value=`yq .env.$key ~/.settings/config.yaml`
+
+    formated_value=${value#"\""}
+    formated_value=${formated_value%"\""}
+
+    variable_name=`echo $key | tr '[:lower:]' '[:upper:]'`
+
+    formated_variable_name=${variable_name#"\""}
+    formated_variable_name=${formated_variable_name%"\""}
+
+    declare $formated_variable_name="$formated_value"
+
+    unset key
+    unset value
+    unset formated_value
+    unset variable_name
+    unset formated_variable_name
+done
+
+unset env_keys
