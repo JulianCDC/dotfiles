dd1f70fd7c5798f4cb244d52a106d8effb5fc574
JulianCDC <me@juliancdc.dev>
2022-07-10 11:00:30 +0200

    dim. 10 juil. 2022 11:00:30 CEST

--- config/polybar/scripts/dpms.sh
+++ config/polybar/scripts/dpms.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
 
-# is_inhibited() {
-#   inhibitors=`systemd-inhibit | grep block | wc -l`
-
-#   if [ "$inhibitors" -gt 0 ]; then
-#     echo "yes"
-#   else
-#     echo "no"
-#   fi
-# }
-
 get_state() {
-  state=`cat ~/.scripts/screensaver/state`
+  state=`config_manager --cli get energy autolock`
 
-  if [ $state == "on" ]; then
-      # if [ `is_inhibited` == "yes" ]; then
-          # echo ""
-      # else
+  if [ $state == "true" ]; then
       echo ""
-      # fi
   else
       echo "%{F#707880}%{F-}"
   fi
@@ -26,6 +12,6 @@ get_state() {
 
 get_state
 
-inotifywait -m -q -e modify ~/.scripts/screensaver/state | while read -r; do
+inotifywait -m -q -e modify ~/.config/JulianCDC | while read -r; do
   get_state
 done
